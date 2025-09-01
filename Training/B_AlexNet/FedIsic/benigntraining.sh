d4b5b5b3dabe4b84b2cc13c9a34678f33ae1d5b5
tofnifdes <adeniran.tofunmi@gmail.com>
2025-09-01 19:28:31 -0400

    reorganized the file structure and corrected the mistake of fedisic from 10 clients to 6 clients

--- Training/B_AlexNet/FedIsic/benigntraining.sh
+++ Training/B_AlexNet/FedIsic/benigntraining.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 echo "Starting server"
-python3 server.py --attribute classLabel &
+python3 server.py --attribute None &
 sleep 3  # Sleep for 3s to give the server enough time to start
-echo "Starting malicious client 0"
-python3 malicious_client.py --partition-id 0 &
-for (( i=1 ; i<10 ; i++ ));  do
+for (( i=0 ; i<6 ; i++ ));  do
     echo "Starting benign client $i"
     python3 benign_client.py --partition-id $i &
 
--- Training/B_AlexNet/FedIsic/fedleaks.sh
+++ Training/B_AlexNet/FedIsic/fedleaks.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "Starting server"
-python3 server.py --attribute None &
+python3 server.py --attribute classLabel &
 sleep 3  # Sleep for 3s to give the server enough time to start
-for (( i=0 ; i<10 ; i++ ));  do
+echo "Starting malicious client 0"
+python3 malicious_client.py --partition-id 0 &
+for (( i=1 ; i<6 ; i++ ));  do
     echo "Starting benign client $i"
     python3 benign_client.py --partition-id $i &
 
